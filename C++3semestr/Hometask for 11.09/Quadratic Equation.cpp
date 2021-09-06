8f86feb0d8bbe7e00a1d8bf15938a56a8dbd8a3d
dionispls <79856632+dionispls@users.noreply.github.com>
2021-09-06 23:12:24 +0300

    Create Quadratic Equation.cpp

---
+++ C++3semestr/Hometask for 11.09/Quadratic Equation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+float a,b,c,det;
+cin >> a >>  b >> c;
+det = b*b - 4*a*c;
+if (det < 0)
+    cout << "No roots:(";
+else if (det == 0)
+    cout << -b/2;
+else
+    cout << (-b+sqrt(det)) /(2*a) << " " << (-b-sqrt(det)) /(2*a);
+return 0;
+}
