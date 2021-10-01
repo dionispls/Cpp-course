62c06b634a657c32b04fcfdec5f50ace20ccfb9a
dionispls <79856632+dionispls@users.noreply.github.com>
2021-10-01 19:18:21 +0300

    Create main.cpp

---
+++ C++3semestr/Hometask for 02.10/Ball/main.cpp
@@ -0,0 +1,16 @@
+#include "Ball.cpp"
+#include "Ball.hpp"
+#include <thread>
+#include <iostream>
+
+int main(){
+    Point a;
+    a.x = 2;
+    a.y = 3;
+    Ball orange_ball(a);
+    orange_ball.push(10, 1);
+    for (double t = 0; t < 10; t+=0.01){
+        orange_ball.fly(t);
+        std::cout << orange_ball.position.x << " " << orange_ball.position.y << "\n";
+            }
+}
